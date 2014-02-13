dd52d9df471f47c4f9924799969d10ba66d6ade1
John Lynch <john.lynch@avantcredit.com>
2014-02-13 00:05:20 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '346c6de90a9bc6fe5d15e8d35b0d29b518e6b177bd53669f05adbc4b244d3189d1c9729a90d844352d12b212c9a1767791ca9e3ad0b81800ef1131156a6ea8e0'
