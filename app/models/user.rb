3b81c25a558fcacfee4717cccb0e6ba1ebe1a773
John Lynch <john.lynch@avantcredit.com>
2014-02-17 14:19:33 -0600

    Implemented Cancan and basic roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  ROLES = %w[member moderator admin]
+    def role?(base_role)
+      role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
+    end  
+  private
+
+  def set_member
+    self.role = 'member'
+  end
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
@@ -9,4 +20,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
   has_many :posts
+
+  before_create :set_member
 end
