4ea9e0e00e3694d7e7425d7c34e6c6dead0037bf
John Lynch <john.lynch@avantcredit.com>
2014-02-18 01:45:53 -0600

    Halfway done with adding topics

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Bloccit::Application.routes.draw do
 
   devise_for :users
 
+  resources :topics
+
   resources :posts
 
   match "about" => 'welcome#about', via: :get
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'faker'
 
+# Create 15 topics
+topics = []
+15.times do
+  topics << Topic.create(
+    name: Faker::Lorem.words(rand(1..10)).join(" "), 
+    description: Faker::Lorem.paragraph(rand(1..4))
+  )
+end
+ 
+
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -10,29 +20,56 @@ rand(4..10).times do
   u.skip_confirmation!
   u.save
 
-  # Note: by calling `User.new` instead of `create`,
-  # we create an instance of a user which isn't saved to the database.
-  # The `skip_confirmation!` method sets the confirmation date
-  # to avoid sending an email. The `save` method updates the database.
-
   rand(5..12).times do
+    topic = topics.first
     p = u.posts.create(
+      topic: topic,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
+    p.update_rank
 
-    rand(3..7).times do
-      p.comments.create(
-        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
-    end
+    topics.rotate!
   end
 end
 
+post_count = Post.count
+User.all.each do |user|
+    rand(30..50).times do
+      p = Post.find(rand(1..post_count))
+      c = user.comments.create(
+        body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"),
+        post: p)
+      c.update_attribute(:created_at, Time.now - rand(600..31536000))
+    end
+end
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: 'john.lynch@avantcredit.com', password: 'helloworld', password_confirmation: 'helloworld')
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
