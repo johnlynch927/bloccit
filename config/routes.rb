67f00dccbf9d4f4507bc3e4a9adaa020e65ea70b
John Lynch <john.lynch@avantcredit.com>
2014-02-25 11:43:51 -0600

    fixed routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Bloccit::Application.routes.draw do
   devise_for :users
 
   resources :topics do
-  resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
     end
